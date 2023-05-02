5d35ec83b8bac47e27263a64f29f631e4a57ab77
SebasProgrammer2021 <sebastian.londonov@uqvirtual.edu.co>
2023-05-02 15:15:18 -0500

    adjustments for hello controller and README.md

--- README.md
+++ README.md
@@ -4,4 +4,14 @@
 2. desde la terminal accedemos a /target donde se genero el jar
 3. con java instalado en la maquina ejecutamos: `java -jar nombreJar.jar`
 4. se inicia la aplicación y podemos usar sus endpoints
-5. 
+
+## Despliegue del aplicativo en railway
+1. crear cuenta en https://railway.app/
+2. conectar la cuenta con github
+3. seleccionar el repo a desplegar
+   (tener en cuenta que el nombre del repo no debe ser mayor a 32 caracteres
+4. dar la opción de -> Deploy now
+5. comienza la construcción y despliegue del proyecto
+
+Notas: en https://railway.app/ tenemos un bono de 5usd o 500hrs de uso del aplicativo desplegado
+
--- src/main/java/co/helloController/springDeploy_helloController/HelloControler.java
+++ src/main/java/co/helloController/springDeploy_helloController/HelloControler.java
@@ -21,10 +21,10 @@ public class HelloControler {
     {
         System.out.println("message = " + message);
 //        System.out.println("envVarUsername = " + envVarUsername);
-        return "Hello Worlde rd nuevo";
+        return "Hello World page new";
     }
 
-    @GetMapping("/bootstrap")
+    @GetMapping("/")
     public String getTemplate(){
 
         return """
@@ -37,7 +37,8 @@ public class HelloControler {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
                   </head>
                   <body>
-                    <h1>Hello, world!</h1>
+                    <h1>Hello!</h1>
+                    <h1>This my example API deployed</h1>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
                   </body>
                 </html>
